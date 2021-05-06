3d54cd8f8c6dcdda3093f5511c2d6ec22d77554c
Ayman Mahmoud <ayman.mahmoud@student-cs.fr>
2021-05-06 17:22:17 +0200

    testing structure added

---
+++ jsprit-io/src/test/java/com/graphhopper/jsprit/io/problem/VehicleParameterReaderTest.java
@@ -0,0 +1,37 @@
+package com.graphhopper.jsprit.io.problem;
+import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem;
+import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem.FleetSize;
+import com.graphhopper.jsprit.core.problem.job.Job;
+import com.graphhopper.jsprit.core.problem.job.Service;
+import com.graphhopper.jsprit.core.problem.job.Shipment;
+import com.graphhopper.jsprit.core.problem.solution.VehicleRoutingProblemSolution;
+import com.graphhopper.jsprit.core.problem.solution.route.activity.DeliverShipment;
+import com.graphhopper.jsprit.core.problem.solution.route.activity.PickupService;
+import com.graphhopper.jsprit.core.problem.solution.route.activity.PickupShipment;
+import com.graphhopper.jsprit.core.problem.solution.route.activity.TourActivity;
+import com.graphhopper.jsprit.core.problem.vehicle.Vehicle;
+import com.graphhopper.jsprit.core.util.Solutions;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class VehicleParameterReaderTest {
+    private InputStream inputStream;
+
+    @Before
+    public void doBefore() {
+        inputStream = getClass().getResourceAsStream("testVehicleParameters.xml");
+    }
+
+    // In the test, if I understood correctly, the reader should take as an argument the object,
+    // the instantiation of the object happens before the call of the readFunction
+    // This is more logical because you can then assert values added to the reader,
+    // you need to make the necessary changes to the code (@link vrpXMLReader)
+}
